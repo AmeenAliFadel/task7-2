a6638a7a53aef78bb31b0cdb637446c93e538a76
AmeenFadel <ameenfadel820@gmail.com>
2024-11-15 18:42:22 +0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,100 @@
-
 import { useState } from 'react'
 import './App.css'
 
-// import Plus from './components/Plus/Plus'
-
 function App() {
-  const [count, setCount] = useState(0)
-  const fun1 = function () {
-      setopenPlusBtn(true)
-      setopenMinusBtn(false)
-      if (count == 0) {
-          setopen(false);
-      }
-      if (count == 9) {
-          setopen(true);
-          setbgColor(true)
-      }
-      if (count < 10) {
-          return (
-              setCount(count + 1)
-          )
-      }
-      if (count == 10) {
-          setopen(false);
-      }
-      if (count == 90) {
-          setopen(true);
-          setbgColor(false)
-      }
-      if (count < 100) {
-          return (
-              setCount(count + 10)
-          )
-      }
-      if (count == 100) {
-          setopen(false);
-      }
-      if (count == 900) {
-          setopen(true);
-          setbgColor(true)
-      }
-      if (count < 1000) {
-          return (
-              setCount(count + 100)
-          )
-      }
-      setopenMinusBtn(true)
-      setopenPlusBtn(false)
-  }
-  const fun2 = function () {
-      setopenPlusBtn(false)
-      setopenMinusBtn(true)
-      if (count == 1) {
-          
-          setbgColor(false)
-      }
-      if (count == 1000) {
-          setopen(false);
-      }
-      if (count == 200) {
-          setopen(true);
-          setbgColor(false)
-      }
-      if (count <= 1000 && count > 100) {
-          return (
-              setCount(count - 100)
-          )
-      }
-      if (count == 100) {
-          setopen(false);
-      }
-      if (count == 20) {
-          setopen(true);
-          setbgColor(true)
-      }
-      if (count <= 100 && count > 10) {
-          return (
-              setCount(count - 10)
-          )
-      }
-      if (count == 10) {
-          setopen(false);
-      }
-      if (count <= 10 && count > 0) {
-          return (
-              setCount(count - 1)
-          )
-      }
-      
-      setopenMinusBtn(false)
-      setopenPlusBtn(true)
-  }
-  const [open, setopen] = useState(true)
-  const [openPlusBtn, setopenPlusBtn] = useState(true)
-  const [openMinusBtn, setopenMinusBtn] = useState(false)
-  const [bgColor ,setbgColor] = useState(false)
-  return (
-    <>
-    <div className={bgColor? 'bg1' : 'bg2'}>
+    const [count, setCount] = useState(0)
+    const [open, setopen] = useState(true)
+    const [openPlusBtn, setopenPlusBtn] = useState(true)
+    const [openMinusBtn, setopenMinusBtn] = useState(false)
+    const [bgColor, setbgColor] = useState(false)
+    const fun1 = function () {
+        setopenPlusBtn(true)
+        setopenMinusBtn(false)
+        if (count == 0) {
+            setopen(false);
+        }
+        if (count == 9) {
+            setopen(true);
+            setbgColor(true)
+        }
+        if (count < 10) {
+            return (
+                setCount(count + 1)
+            )
+        }
+        if (count == 10) {
+            setopen(false);
+        }
+        if (count == 90) {
+            setopen(true);
+            setbgColor(false)
+        }
+        if (count < 100) {
+            return (
+                setCount(count + 10)
+            )
+        }
+        if (count == 100) {
+            setopen(false);
+        }
+        if (count == 900) {
+            setopen(true);
+            setbgColor(true)
+            setopenMinusBtn(true)
+            setopenPlusBtn(false)
+        }
+        if (count < 1000) {
+            return (
+                setCount(count + 100)
+            )
+        }
+    }
+    const fun2 = function () {
+        setopenPlusBtn(false)
+        setopenMinusBtn(true)
+        if (count == 1) {
+            setbgColor(false)
+            setopenMinusBtn(false)
+            setopenPlusBtn(true)
+            setopen(true);
+        }
+        if (count == 1000) {
+            setopen(false);
+        }
+        if (count == 200) {
+            setopen(true);
+            setbgColor(false)
+        }
+        if (count <= 1000 && count > 100) {
+            return (
+                setCount(count - 100)
+            )
+        }
+        if (count == 100) {
+            setopen(false);
+        }
+        if (count == 20) {
+            setopen(true);
+            setbgColor(true)
+        }
+        if (count <= 100 && count > 10) {
+            return (
+                setCount(count - 10)
+            )
+        }
+        if (count == 10) {
+            setopen(false);
+        }
+        if (count <= 10 && count > 0) {
+            return (
+                setCount(count - 1)
+            )
+        }
+    }
+
+    return (
+        <>
+            <div className={bgColor ? 'bg1' : 'bg2'}>
                 <div className="card">
                     <p className='p-area'>count is {count}</p>
                 </div>
@@ -115,8 +112,8 @@ function App() {
                     </div>
                 </div>
             </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
